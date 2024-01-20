4ce20d01744535ba6a9bc618daca272faf47989e
Rama Krishna Kandukuri <rkandukuri@Ramas-MacBook-Pro.local>
2024-01-20 18:48:43 +0100

    Built site locally

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Physics-Based Rigid Body Object Tracking and Friction Filtering From RGB-D Videos",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/home/projects/ekfphys/",
+        "teaser": null
+      },{
+        "title": "Learning to Identify Physical Parameters from Video Using Differentiable Physics",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/home/projects/videodiffphys/",
+        "teaser": null
+      }]
