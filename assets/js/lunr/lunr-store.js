9f4fe5d84d47eae50927368cb11e5f51351a88cd
Rama Krishna Kandukuri <prasadkrkp@gmail.com>
2024-01-20 23:58:59 +0100

    Update project description for ekfphys project

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "updates",
+        "excerpt":"   Joined Polybot as robot software engineer.   Our paper, Physics-Based Rigid Body Object Tracking and Friction Filtering From RGB-D Videos accepted in 3DV 2024. Check here.  ","categories": [],
+        "tags": [],
+        "url": "/recent_updates/",
+        "teaser": null
+      },{
         "title": "Physics-Based Rigid Body Object Tracking and Friction Filtering From RGB-D Videos",
         "excerpt":" ","categories": [],
         "tags": [],
